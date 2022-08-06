4277c75fce0dc969264b5390fcf53a42a1c18792
cirill98 <100434317+cirill98@users.noreply.github.com>
2022-08-06 22:46:45 +0500

    второе задание

---
+++ dz_1_2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int num4 = 0;
+if (num1 > num2)
+{
+    num4 = num1;
+}
+else
+{
+    num4 = num2;
+}
+if (num4 > num3)
+{
+    Console.WriteLine(num4);
+}
+else
+{
+    Console.WriteLine(num3);
+}
