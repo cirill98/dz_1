9cc2fa9f646df5625817377af43a9d7de9b8aa50
cirill98 <100434317+cirill98@users.noreply.github.com>
2022-08-06 14:28:04 +0500

    сделали первое задание

---
+++ dz_1_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.Write("max = ");
+    Console.WriteLine(num1);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(num2);
+}
