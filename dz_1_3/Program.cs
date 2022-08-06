f5f475a50eb50bde5d0ca49313ef2e6c188c4fef
cirill98 <100434317+cirill98@users.noreply.github.com>
2022-08-06 22:47:11 +0500

    третье задание

---
+++ dz_1_3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if ( num % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
